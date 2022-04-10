5d5b750e4d9bca381163d6882c67febc0060371e
Bitchard <richard@agreekhouse.com>
2022-04-10 06:33:57 -0500

    created the methods to retrieve the hour, minute, secound from the current time variable.

---
+++ script.js
@@ -0,0 +1,19 @@
+//function to display the time on the page
+
+$(document).ready(function() {
+    function displayTime() {
+
+    }
+});
+
+//variable to store the current time
+function displayTime() {
+    var currentTime = new Date();
+//created the method so getHours,minutes and secounds they will extract hours, 
+//miutes and secounds from current time variable.
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var secounds = currentTime.getSecounds();
+}
+//these steps will get computer to fill current time
+
